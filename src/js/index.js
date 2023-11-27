4ff301e34cd29d80dde5a5607f2238c01aaa5d85
PabloC2A <116039498+PabloC2A@users.noreply.github.com>
2023-11-27 15:30:04 -0500

    Frontend y Backend
    Se agrego la pagina para login y el script para autenticacion, ademas se puso la pagina de dashboard

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,12 @@
 function Login() {
-    const usuario = document.getElementById('username').value;
-    const contrasenia = document.getElementById('password').value;
-
-    if (usuario === 'pepito' && contrasenia === '123') {
-        alert('Exitoso')
-        window.location.href = '/views/dashboard.html';
-    } else {
-        alert('Incorrecto')
+    const usuario = document.getElementById("username").value;
+    const contrasenia = document.getElementById("password").value;
+    
+    if(usuario === "pepito" && contrasenia === "123456789") {
+        alert("Exitoso");
+        window.location.href = "../views/dashboard.html";
+    }else {
+        alert("Incorrecto")
+        document.createElement();
     }
 }
