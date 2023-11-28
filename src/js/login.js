134af268371e8491726ec70a515290574c3216ea
PabloC2A <116039498+PabloC2A@users.noreply.github.com>
2023-11-28 15:20:27 -0500

    Actualizacion de Proyecto 2.0

--- src/js/login.js
+++ src/js/login.js
@@ -1,12 +1,22 @@
-function Login() {
-    const usuario = document.getElementById("username").value;
-    const contrasenia = document.getElementById("password").value;
-    
-    if(usuario === "pepito" && contrasenia === "123456789") {
-        alert("Exitoso");
-        window.location.href = "../views/dashboard.html";
-    }else {
-        alert("Incorrecto")
-        document.createElement();
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+  
+    const usuarios = [
+      { username: 'pablo', password: '123456789' },
+      { username: 'denis', password: '123456789' },
+      { username: 'augusto', password: '123456789' }
+    ];
+  
+    const usuario = document.getElementById('username').value;
+    const contrasenia = document.getElementById('password').value;
+  
+    // Verificar credenciales
+    const user = usuarios.find(user => user.username === usuario && user.password === contrasenia);
+  
+    if (user) {
+      window.location.href = '../views/inicio.html';
+    } else {
+      alert('Incorrecto');
     }
-}
+});
+  
